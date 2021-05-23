b399a62ab511eeaeb01ece86a3a88e8ad81f80e3
Oliver Parczyk <oliver.parczyk@mailbox.tu-dresden.de>
2021-05-23 18:03:43 +0200

    Add connect function

--- src/main/java/de/olipar/iAT_plc4x_project/OPCUAClient.java
+++ src/main/java/de/olipar/iAT_plc4x_project/OPCUAClient.java
@@ -10,6 +10,8 @@ public class OPCUAClient {
 
 	private String server_url;
 	private Logger logger;
+	private boolean is_connected = false;
+	private PlcConnection plcConnection = null;
 
 	public OPCUAClient(String server_url, Logger logger) {
 		if (!server_url.startsWith("opcua:")) {
@@ -19,4 +21,14 @@ public class OPCUAClient {
 		this.logger = logger;
 	}
 
+	public void connect() {
+		try {
+			PlcConnection plcConnection = new PlcDriverManager().getConnection(server_url);
+			logger.info("Connected to OPC UA server");
+			this.is_connected = true;
+			this.plcConnection = plcConnection;
+		} catch (PlcConnectionException e) {
+			logger.severe("Connection to " + server_url + " failed: " + e.getMessage());
+		}
+	}
 }
