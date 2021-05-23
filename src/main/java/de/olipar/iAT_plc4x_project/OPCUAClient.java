63f17d544a877ecb63cf5af1b9e611bad777f47c
Oliver Parczyk <oliver.parczyk@mailbox.tu-dresden.de>
2021-05-23 18:47:54 +0200

    Add adding read requests for nodes

--- src/main/java/de/olipar/iAT_plc4x_project/OPCUAClient.java
+++ src/main/java/de/olipar/iAT_plc4x_project/OPCUAClient.java
@@ -1,16 +1,21 @@
 package de.olipar.iAT_plc4x_project;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.logging.Logger;
 
 import org.apache.plc4x.java.PlcDriverManager;
 import org.apache.plc4x.java.api.PlcConnection;
 import org.apache.plc4x.java.api.exceptions.PlcConnectionException;
+import org.apache.plc4x.java.api.messages.PlcReadRequest;
+import org.javatuples.Pair;
 
 public class OPCUAClient {
 
 	private String server_url;
 	private Logger logger;
 	private PlcConnection plcConnection = null;
+	private List<Pair<String, String>> readItemList;
 
 	public OPCUAClient(String server_url, Logger logger) {
 		if (!server_url.startsWith("opcua:")) {
@@ -18,6 +23,7 @@ public class OPCUAClient {
 		}
 		this.server_url = server_url;
 		this.logger = logger;
+		readItemList = new LinkedList();
 	}
 
 	public void connect() {
@@ -28,5 +34,21 @@ public class OPCUAClient {
 		} catch (PlcConnectionException e) {
 			logger.warning("Connection to " + server_url + " failed: " + e.getMessage());
 		}
+	}
+
+	public void addReadItem(String name, String nodeID) {
+		if (name == null) {
+			throw new NullPointerException("Item name MUST NOT be null!");
+		}
+		if (name.isEmpty()) {
+			throw new IllegalArgumentException("Item name MUST NOT be empty");
+		}
+		if (nodeID == null) {
+			throw new NullPointerException("nodeID MUST NOT be null!");
+		}
+		if (nodeID.isEmpty()) {
+			throw new IllegalArgumentException("nodeID MUST NOT be empty");
+		}
+		readItemList.add(new Pair<String, String>(name, nodeID));
 	}
 }
