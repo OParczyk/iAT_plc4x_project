7ad02259c4db2cb3ebb50dede891744d31a50980
Oliver Parczyk <oliver.parczyk@mailbox.tu-dresden.de>
2021-05-23 17:45:54 +0200

    Add setup of opc ua server url

--- src/main/java/de/olipar/iAT_plc4x_project/OPCUAClient.java
+++ src/main/java/de/olipar/iAT_plc4x_project/OPCUAClient.java
@@ -2,14 +2,21 @@ package de.olipar.iAT_plc4x_project;
 
 import java.util.logging.Logger;
 
+import org.apache.plc4x.java.PlcDriverManager;
+import org.apache.plc4x.java.api.PlcConnection;
+import org.apache.plc4x.java.api.exceptions.PlcConnectionException;
+
 public class OPCUAClient {
 
 	private String server_url;
+	private Logger logger;
 
 	public OPCUAClient(String server_url, Logger logger) {
 		if (!server_url.startsWith("opcua:")) {
 			throw new IllegalArgumentException("OPC UA server URL must start with 'opcua:'!");
 		}
 		this.server_url = server_url;
+		this.logger = logger;
 	}
+
 }
