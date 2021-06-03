73092a13abec70869aab0d08be4ac006227ddc5a
Oliver Parczyk <oliver.parczyk@mailbox.tu-dresden.de>
2021-06-03 23:45:39 +0200

    Refactor all translation tasks into translator class

--- src/main/java/de/olipar/iAT_plc4x_project/Main.java
+++ src/main/java/de/olipar/iAT_plc4x_project/Main.java
@@ -1,63 +1,29 @@
 package de.olipar.iAT_plc4x_project;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.Timer;
-import java.util.TimerTask;
 import java.util.logging.Logger;
 
-import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
-import org.eclipse.paho.client.mqttv3.MqttCallback;
-import org.eclipse.paho.client.mqttv3.MqttMessage;
-import org.javatuples.Pair;
-
-public class Main implements MqttCallback {
+public class Main {
 
 	private static Logger logger;
 	private static OPCUAClient opc;
 	private static MyMQTTClient mqtt;
 	private static String mqttTopicPrefix = "iat/test/";
 	private static final long TRANSLATE_CYCLE_MS = 2000;
-	private static Map<String, String> topicToNodeIDMap;
 
 	public static void main(String[] args) {
 		logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
 		opc = new OPCUAClient("opcua:tcp://localhost:12686/", logger);
 		opc.connect();
 		mqtt = new MyMQTTClient("tcp://test.mosquitto.org:1883", logger);
-		mqtt.setCallback(new Main());
+		Translator translator = new Translator(mqtt, mqttTopicPrefix, opc, logger);
+		mqtt.setCallback(translator);
 		mqtt.connect();
-
-		topicToNodeIDMap = new HashMap<String, String>();
 
 		opc.addReadItem("simulatorwert", "ns=2;s=items-lrBeltDriveSpeed:UDINT");
 		// Get OPC values and sent to MQTT every TRANSLATE_CYCLE_MS
 		Timer timer = new Timer();
-		Translator translator = new Translator(mqtt, mqttTopicPrefix, opc);
 		timer.schedule(translator, 0, TRANSLATE_CYCLE_MS);
-
-	}
-
-	private void subscribeToChannel(String topicSuffix, String nodeID) {
-		topicToNodeIDMap.put(mqttTopicPrefix + topicSuffix, nodeID);
-		mqtt.subscribe(mqttTopicPrefix + topicSuffix);
-	}
-
-	public void connectionLost(Throwable cause) {
-		logger.warning("MQTT lost connection, retrying...");
-		mqtt.connect();
-	}
-
-	public void messageArrived(String topic, MqttMessage message) throws Exception {
-		logger.info("Received message on " + topic + ": " + new String(message.getPayload()));
-
-		// TODO: Check if casting is necessary.
-		opc.writeValue(topic, topicToNodeIDMap.get(mqttTopicPrefix + topic), message);
-	}
-
-	public void deliveryComplete(IMqttDeliveryToken token) {
-		return;
 
 	}
 
--- src/main/java/de/olipar/iAT_plc4x_project/Translator.java
+++ src/main/java/de/olipar/iAT_plc4x_project/Translator.java
@@ -1,16 +1,24 @@
 package de.olipar.iAT_plc4x_project;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.TimerTask;
+import java.util.logging.Logger;
 
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.javatuples.Pair;
 
-public class Translator extends TimerTask {
+public class Translator extends TimerTask implements MqttCallback {
 	private MyMQTTClient mqtt;
 	private String mqttTopicPrefix;
 	private OPCUAClient opc;
+	private Logger logger;
+	private Map<String, String> topicToNodeIDMap;
 
-	public Translator(MyMQTTClient mqtt, String mqttTopicPrefix, OPCUAClient opc) {
+	public Translator(MyMQTTClient mqtt, String mqttTopicPrefix, OPCUAClient opc, Logger logger) {
 		if (mqtt == null) {
 			throw new NullPointerException("MQTT client MUST NOT be null!");
 		}
@@ -20,9 +28,19 @@ public class Translator extends TimerTask {
 		if (opc == null) {
 			throw new NullPointerException("OPC client MUST NOT be null!");
 		}
+		if (logger == null) {
+			throw new NullPointerException("Logger MUST NOT be null!");
+		}
 		this.mqtt = mqtt;
 		this.mqttTopicPrefix = mqttTopicPrefix;
 		this.opc = opc;
+		this.logger = logger;
+		topicToNodeIDMap = new HashMap<String, String>();
+	}
+
+	public void subscribeToChannel(String topicSuffix, String nodeID) {
+		topicToNodeIDMap.put(mqttTopicPrefix + topicSuffix, nodeID);
+		mqtt.subscribe(mqttTopicPrefix + topicSuffix);
 	}
 
 	@Override
@@ -32,5 +50,22 @@ public class Translator extends TimerTask {
 				mqtt.publish(mqttTopicPrefix + values.getValue0(), value.getBytes(), 2);
 			}
 		}
+	}
+
+	public void connectionLost(Throwable cause) {
+		logger.warning("MQTT lost connection, retrying...");
+		mqtt.connect();
+	}
+
+	public void messageArrived(String topic, MqttMessage message) throws Exception {
+		logger.info("Received message on " + topic + ": " + new String(message.getPayload()));
+
+		// TODO: Check if casting is necessary.
+		opc.writeValue(topic, topicToNodeIDMap.get(mqttTopicPrefix + topic), message);
+	}
+
+	public void deliveryComplete(IMqttDeliveryToken token) {
+		return;
+
 	}
 }
